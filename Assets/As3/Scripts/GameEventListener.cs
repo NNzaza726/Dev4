0dab76247cd115a858ef78944d23d392c41023f8
unknown <atimak391@gmail.com>
2024-12-24 14:27:55 +0700

    Add Program
    24/12/2024

---
+++ Assets/As3/Scripts/GameEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Samarnggoon.GameDev4.EventDrivenArch
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent gameEvent;
+        public UnityEvent response;
+
+        public void OnEventRaised() => response?.Invoke();
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+    }
+}
